56ff645ae8b3e720b7df9974f0018d8b437f3c22
Glich29 <sbislava@gmail.com>
2023-12-19 00:22:25 +0300

    A method was added in EComDataRestConfig to expose entity ids.

--- src/main/java/org/sbislava/ecomshop/config/EComDataRestConfig.java
+++ src/main/java/org/sbislava/ecomshop/config/EComDataRestConfig.java
@@ -1,5 +1,7 @@
 package org.sbislava.ecomshop.config;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
 import org.sbislava.ecomshop.entity.Product;
 import org.sbislava.ecomshop.entity.ProductCategory;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +10,24 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
-public class eComDataRestConfig implements RepositoryRestConfigurer {
+public class EComDataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
     private final static HttpMethod[] THE_UNSUPPORTED_ACTIONS = {
             HttpMethod.PUT,
             HttpMethod.POST,
             HttpMethod.DELETE
     };
+
+    public EComDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
@@ -23,6 +36,9 @@ public class eComDataRestConfig implements RepositoryRestConfigurer {
 
         //disable HTTP methods for ProductCategory: PUT, POST and DELETE
         disabledExposureConfiguration(config, ProductCategory.class);
+
+        //call internal helper method
+        exposeIds(config);
     }
 
     private void disabledExposureConfiguration(RepositoryRestConfiguration config, Class<?> clazz) {
@@ -30,5 +46,13 @@ public class eComDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(clazz)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(THE_UNSUPPORTED_ACTIONS))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(THE_UNSUPPORTED_ACTIONS));
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class<?>> entityClasses = new ArrayList<>();
+        entities.forEach(entity -> entityClasses.add(entity.getJavaType()));
+        Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
